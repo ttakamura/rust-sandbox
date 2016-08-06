410e229d0e019a5aac5827e1ba02beccd17706f5
Tatsuya Takamura <takamura@tonchidot.com>
2016-08-06 13:19:29 +0900

    The Rust Book - 4.2 variable

--- src/syntax/main.rs
+++ src/syntax/main.rs
@@ -1,9 +1,23 @@
 fn main(){
-    let x = 5;
-    println!("x = {}", x);
+    let x: i32 = 5;
+    println!("x = {:?}", x);
+
+    // Error - x is immutable
+    // x = 10;
+
+    // confirm type
+    // let hoge:() = x;
 
     let (x, y) = (10, 20);
-    println!("x = {}", x);
+    println!("x = {} y = {}", x, y);
+
+    {
+        let y: i32 = 400;
+        let z: i32 = 30;
+        println!("X = {} Y = {} Z = {}", x,y,z)
+    }
+    // Error - z is out of the scope
+    //println!("X = {} Y = {} Z = {}", x,y,z)
 
     println!("Done");
 }
